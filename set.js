a50d2ce62d1d08fcd80f05a648e522c5e994f21f
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-04-19 17:53:26 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIX || "~",
-    OWNER_NAME: process.env.OWNER_NAME || "IBRAHIM-TECH",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "254751284190",              
+    OWNER_NAME: process.env.OWNER_NAME || "KEITH-TECH",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254748387615",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
     BOT : process.env.BOT_NAME || 'FLASH-MD',
