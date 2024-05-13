a1d4aa1ff6cf5c8c66a7a5508bc0cbc761cb76f5
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-05-13 20:23:12 +0300

    ALPHA

--- commandes/alive.js
+++ commandes/alive.js
@@ -71,7 +71,7 @@ else {
 }
 
     } else {
-        if(!superUser) { repondre("there is no alive for this bot") ; return};
+        if(!superUser) { repondre("there is no alive for this bot idiot") ; return};
 
       await   repondre("You have not yet saved your alive, to do this;  enter after alive your message and your image or video link in this context: .alive message;lien");
          repondre("don't do fake things :)")
