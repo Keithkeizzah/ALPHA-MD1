8dc977b0efc6b12d946f7d5df88924e6eb4fdefd
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-06-16 20:09:29 +0300

    ༆✞

--- commandes/test.js
+++ commandes/test.js
@@ -4,7 +4,7 @@ const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "repo", reaction: "👍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝐖𝐇𝐀𝐓"𝐒 𝐔𝐏 𝐌𝐘 𝐆𝐄𝐄 𝐓𝐇𝐈𝐒 𝐈𝐒 𝐀𝐋𝐏𝐇𝐀 𝐁𝐎𝐓  \n\n ' + "𝐅𝐎𝐑𝐊 𝐓𝐇𝐄 𝐑𝐄𝐏𝐎 𝐓𝐎 𝐃𝐄𝐏𝐋𝐎𝐘 𝐘𝐎𝐔𝐑 𝐎𝐖𝐍 𝐁𝐎𝐓 *𝗞𝗘𝗜𝗧𝗛-𝗧𝗘𝗖𝗛 𝗥𝗘𝗣𝗢.*";
-    let d = ' https://github.com/keithkeizzah/ALPHA-MD';
+    let d = ' https://github.com/keithkeizzah/ALPHA-MD1';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/115aeb8879444b3fa8568.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
