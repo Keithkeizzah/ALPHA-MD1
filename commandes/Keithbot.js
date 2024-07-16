ba034a57665406da7d33417b5184bc49cdd7024f
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-07-16 14:53:57 +0300

    ༆✞

--- commandes/Keithbot.js
+++ commandes/Keithbot.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "keith", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '```𝐌𝐲 𝐨𝐰𝐧𝐞𝐫 𝐧𝐚𝐦𝐞 𝐛𝐫𝐨/𝐬𝐢𝐳 𝐰𝐡𝐢𝐜𝐡𝐞𝐯𝐞𝐫 😉𝐣𝐮𝐬𝐭 𝐟𝐨𝐥𝐥𝐨𝐰 𝐦𝐲 𝐦𝐚𝐬𝐭𝐞𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐚𝐧𝐝 𝐬𝐡𝐚𝐫𝐞 𝐧𝐨𝐰 𝐢𝐟 𝐲𝐨𝐮 𝐫𝐞𝐚𝐥𝐥𝐲 𝐥𝐢𝐤𝐞 𝐡𝐢𝐬 𝐡𝐚𝐧𝐝𝐰𝐨𝐫𝐤```\n\n ' + "*https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47*";
+    let z = '```𝐓𝐡𝐚𝐭 𝐢𝐬 𝐦𝐲 𝐨𝐰𝐧𝐞𝐫 𝐧𝐚𝐦𝐞 𝐛𝐫𝐨/𝐬𝐢𝐳! 𝐰𝐡𝐢𝐜𝐡𝐞𝐯𝐞𝐫 𝐛𝐮𝐝𝐝𝐢𝐞😉𝐣𝐮𝐬𝐭 𝐟𝐨𝐥𝐥𝐨𝐰 𝐦𝐲 𝐦𝐚𝐬𝐭𝐞𝐫 𝐜𝐡𝐚𝐧𝐧𝐞𝐥 𝐚𝐧𝐝 𝐬𝐡𝐚𝐫𝐞 𝐧𝐨𝐰 𝐢𝐟 𝐲𝐨𝐮 𝐫𝐞𝐚𝐥𝐥𝐲 𝐥𝐢𝐤𝐞 𝐡𝐢𝐬 𝐡𝐚𝐧𝐝𝐰𝐨𝐫𝐤```\n\n ' + "*https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47*";
     let d = ' #𝐌𝐚𝐤𝐞 𝐬𝐮𝐫𝐞 𝐲𝐨𝐮 𝐟𝐨𝐥𝐥𝐨𝐰😜';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/ac64eb9543937fe3baaf3.jpg';
