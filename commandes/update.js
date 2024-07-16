2bc047d200543145c975c48a8dcdaf058c1b72f2
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-07-16 23:06:04 +0300

    ༆✞

---
+++ commandes/update.js
@@ -0,0 +1,127 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "wagroup", reaction: "👁", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '𝐀𝐋𝐏𝐇𝐀-𝐌𝐃\n\n👋Hello,,Click The link below to Join 𝐊𝐄𝐈𝐓𝐇-𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐆𝐑𝐎𝐔𝐏 \n\n';
+    let d = 'https://chat.whatsapp.com/D8iHlD3Nk70ITriRlGlkBW\n\nJoin the group and wait for updates from the admin,Thank you. \n\n\n> *ʀᴇɢᴀʀᴅs ᴍʏ ᴏᴡɴᴇʀ Kᴇɪᴛʜ🤝*';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "channel", reaction: "👁", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '𝐀𝐋𝐏𝐇𝐀-𝐌𝐃\n\nHello👋,Click on the link below and follow 𝐊𝐄𝐈𝐓𝐇-𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐅𝐎𝐑 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 \n';
+    let d = '[https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47]\nMake sure you follow.Thank you\n\n\n> *ʀᴇɢᴀʀᴅs ᴍʏ ᴏᴡɴᴇʀ Kᴇɪᴛʜ🤝*';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "developer", reaction: "🎗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *𝗕𝗘𝗟𝗢𝗪 𝗜𝗦 𝗧𝗛𝗘 𝗗𝗘𝗩 𝗗𝗘𝗧𝗔𝗜𝗟𝗦* \n\n☢ *𝗡𝗔𝗠𝗘* : Kᴇɪᴛʜ Kᴇɪᴢᴢᴀʜ \n☢ *𝗔𝗚𝗘* :  20 ʏᴇᴀʀs\n☢ *𝗦𝗘𝗫* : Mᴀʟᴇ\n☢ *𝗥𝗘𝗟𝗜𝗚𝗜𝗢𝗡* : Cʜʀɪsᴛɪᴀɴ\n☢ *𝗡𝗔𝗧𝗜𝗢𝗡𝗔𝗟𝗜𝗧𝗬* :Kᴇɴʏᴀɴ 🇰🇪\n☢ * 𝗘𝗣𝗟 𝗧𝗘𝗔𝗠* :Aʟᴡᴀʏs Aʀsᴇɴᴀʟ ᴛʜᴇ ɢᴜɴɴᴇʀs😉\n☢ *𝗥𝗘𝗔𝗟 𝗟𝗜𝗙𝗘* :Bᴜsɪɴᴇss ᴍᴀɴ .\n☢ *𝗛𝗢𝗕𝗕𝗬* : Wᴏʀᴋɪɴɢ ᴏɴ ᴍʏ ᴄᴏᴅɪɴɢ ᴘʀᴏᴊᴇᴄᴛ ᴀᴛ ᴀɴʏ ғʀᴇᴇ ᴛɪᴍᴇ\n☢ *𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗞𝗘𝗜𝗧𝗛 𝗙𝗢𝗥 𝗠𝗢𝗥𝗘* :👇 👇 \n';
+    let d = '☠ https://wa.me/254748387615\n☠ https://wa.me/254796299159\n\n> *𝐁𝐄 𝐖𝐈𝐓𝐇 𝐆𝐎𝐃 𝐅𝐀𝐌*.\n>*ʀᴇɢᴀʀᴅs ᴍʏ ᴏᴡɴᴇʀ Kᴇɪᴛʜ🤝*  ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "test", reaction: "✅", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *☹︎ ALPHA-MD IS ACTIVE ☹︎*\n';
+    let d = '> REGARDS KEITH';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "bot", reaction: "🤖", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *𝐀𝐋𝐏𝐇𝐀-𝐌𝐃 𝐅𝐄𝐀𝐓𝐔𝐑𝐄𝐒* \n\n 1.Auto view status✅\n 2.Always online✅\n 3.A1 chat gpt enabled \n 4.Download songs/images✅\n 5.Open view once photos✅\n 6.Antidelete messages✅\n 7.Auto block contacts✅\n 8.Auto bluetick✅\n 8.Movie cmds to download movies and series✅\n';
+    let d = '>They can be turned on and off if you like\n\n*THANK YOU FOR LOVING ALPHA-MD*';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "lgbtq", reaction: "🏳️‍🌈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *𝐎𝐡𝐡 𝐆𝐨𝐝 𝐲𝐨𝐮𝐫 𝐝𝐚𝐮𝐠𝐡𝐭𝐞𝐫/𝐬𝐨𝐧 𝐡𝐞𝐫𝐞 𝐢𝐬 𝐆𝐚𝐲/𝐋𝐞𝐬𝐛𝐢𝐚𝐧 𝐰𝐡𝐢𝐜𝐡𝐞𝐯𝐞𝐫😴 𝐚𝐧𝐝 𝐥𝐢𝐤𝐞 𝐭𝐡𝐞𝐫𝐞 𝐰𝐚𝐲 𝐨𝐟 𝐥𝐢𝐟𝐞 *\n';
+    let d = '𝐑𝐞𝐜𝐞𝐢𝐯𝐞 𝐏𝐑𝐀𝐘𝐄𝐑𝐒 𝐢𝐧 𝗝𝐄𝐒𝐔𝐒 𝐍𝐀𝐌𝐄  `𝗔𝗠𝗘𝗡`';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "check", reaction: "🗿", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *𝗔𝗟𝗣𝗛𝗔-𝗠𝗗 𝗨𝗣𝗗𝗔𝗧𝗘𝗦*\n\n';
+    let d = 'Checking for updates...⏳\n\nUpdating new version...⏳\n\ndone....✅\n\n> *𝗔𝗟𝗣𝗛𝗔-𝗠𝗗 VERSION SUCCESSFULLY INSTALLED...* ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "version", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' * _𝐘𝐨𝐮 𝐚𝐫𝐞 𝐮𝐬𝐢𝐧𝐠 𝐀𝐋𝐏𝐇𝐀-𝐌𝐃 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 2.0.0 𝐥𝐚𝐭𝐞𝐬𝐭 2024*\n\n';
+    let d = '✅latest version installed...';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "bugmenu", reaction: "🐛", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '┌─❒-𝐀𝐋𝐏𝐇𝐀-𝐌𝐃 𝐁𝐔𝐆 𝐌𝐄𝐍𝐔❒⁠⁠⁠\n│╭─────────────···\n┴│▸\n⬡│▸ ʙᴜɢ\n⬡│▸ ʙᴜɢᴠ\n⬡│▸ ʙᴜɢʀᴇᴀᴄᴛ\n⬡│▸ ᴀᴜᴅɪᴏʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢᴠ\n┬│▸\n│╰────────────···▸▸\n';
+    let d = '└───────────────···▸';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "news", reaction: "🇰🇪", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *❒⁠⁠⁠𝐓𝐡𝐞 𝐛𝐞𝐥𝐨𝐰 𝐥𝐢𝐧𝐤 𝐢𝐬 𝐨𝐟 𝐭𝐡𝐞 𝐋𝐚𝐭𝐞𝐬𝐭 𝐊𝐞𝐧𝐲𝐚𝐧 𝐍𝐞𝐰𝐬  ❒⁠⁠⁠⁠*\n\n𝐂𝐥𝐢𝐜𝐤 𝐢𝐭 𝐚𝐧𝐝 𝐣𝐨𝐢𝐧 𝐦𝐲 𝐩𝐚𝐠𝐞 *❒⁠⁠⁠⁠𝐊𝐄𝐈𝐓𝐇 𝐓𝐑𝐄𝐍𝐃𝐈𝐍𝐆 𝐍𝐄𝐖𝐒❒* official⁠⁠⁠⁠ WhatsApp Channel\nHelp us grow and reject that piece of shiet.\n\n';
+    let d = 'https://www.facebook.com/profile.php?id=61560520487051  𝐚𝐧𝐝 https://x.com/keithkeizzah?t=yC5xptJCzxf5RJRBE1qjBA&s=08 ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/db98050a014b44c1f2706.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Djalega++*'
+      let varmess=z+d
+      var img='https://wallpapercave.com/uwp/uwp3842939.jpeg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
