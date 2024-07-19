bf6b0ab4c0cb3b2c06c55b70303a2aefdd059850
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-07-19 23:23:24 +0300

    ༆✞

--- commandes/repos.js
+++ commandes/repos.js
@@ -1,1 +1,81 @@
-l
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+zokou({ nomCom: "repo", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+Hello ${nomAuteurMessage},,,👋This is 𝐀𝐋𝐏𝐇𝐀-𝐌𝐃 the best bot in the universe developed by Kᴇɪᴛʜ Kᴇɪᴢᴢᴀʜ,,fork and give a star 🌟 to my repo
+╭─────────────────☆
+│❍ *Developer* :Keithkeizzah
+│❍ *Repo* : https://github.com/keithkeizzah/ALPHA-MD1
+│
+│❍ *Session* : https://keith-sessions-pi5z.onrender.com
+│
+│❍▸ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+│❍ *Youtube* : https://youtube.com/@keithkeizzah
+│❍ *Channel* : https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47
+│
+│❍ *Telegram* : https://t.me/keithmd
+│
+> Regards keithkeizzah
+╰──────────────────☆`;
+let menuMsg = `
+     𝐑𝐞𝐠𝐚𝐫𝐝𝐬 𝐊𝐄𝐈𝐓𝐇 𝐊𝐄𝐈𝐙𝐙𝐀𝐇`;
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *keithkeizzah*, déveloper Keith Tech" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *keithkeizzah*, déveloper Keith Tech" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+}); 
