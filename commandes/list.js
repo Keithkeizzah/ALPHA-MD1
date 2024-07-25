5d363a7e03fd20d39b6cc3f3338670dfe6e537d5
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-07-25 22:25:08 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/list.js
+++ commandes/list.js
@@ -6,7 +6,7 @@ const os = require("os");
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
 
-zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "list", categorie: "General" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
     let { cm } = require(__dirname + "/../framework//zokou");
     var coms = {};
