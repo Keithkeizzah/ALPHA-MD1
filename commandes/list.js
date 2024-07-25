b5c5e769c2d4d55c3d183daae345ddd6d09daf9c
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-07-25 23:55:24 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/list.js
+++ commandes/list.js
@@ -1,177 +1,100 @@
 const util = require('util');
-
 const fs = require('fs-extra');
-
 const { zokou } = require(__dirname + "/../framework/zokou");
-
 const { format } = require(__dirname + "/../framework/mesfonctions");
-
 const os = require("os");
-
 const moment = require("moment-timezone");
-
 const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
 
-
-
-zokou({ nomCom: "list", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
-
+zokou({ nomCom: "list", categorie: "General" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-
     let { cm } = require(__dirname + "/../framework//zokou");
-
     var coms = {};
-
     var mode = "public";
-
     
-
     if ((s.MODE).toLocaleLowerCase() != "yes") {
-
         mode = "private";
-
     }
 
 
-
-
-
     
-
-
 
     cm.map(async (com, index) => {
-
         if (!coms[com.categorie])
-
             coms[com.categorie] = [];
-
         coms[com.categorie].push(com.nomCom);
-
     });
 
-
-
-    moment.tz.setDefault(s.TZ);
-
-
+    moment.tz.setDefault('Etc/GMT');
 
 // Créer une date et une heure en GMT
-
 const temps = moment().format('HH:mm:ss');
-
 const date = moment().format('DD/MM/YYYY');
 
-
-
   let infoMsg =  `
-
-  ╭━━━〔𝐀𝐋𝐏𝐇𝐀-𝐌𝐃 〕━━━┈⊷
-  ┃✵╭──────────────
-  ┃✵│    :𝐌𝐎𝐃𝐄 ${mode}
-  ┃✵│𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 :keithkeizzah
-  ┃✵│𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : baileys
-  ┃✵│️𝐏𝐫𝐞𝐟𝐢𝐱 : ${s.PREFIXE}
-  ┃✵│️𝐃𝐚𝐭𝐞 : ${date}
-  ┃✵│𝐓𝐢𝐦𝐞 : ${temps}
-  ┃✵│𝐏𝐥𝐮𝐠𝐢𝐧𝐬 : 847
-  ┃✵│ 𝐑𝐚𝐦: 26.11GB/120.9 GB
-  ┃✵│𝐓𝐡𝐞𝐦𝐞 : *Keith*
-  ┃✵╰──────────────
-  ╰━━━━━━━━━━━━━━━┈⊷\n\n`;
-
+╭━━━ 〔 ${s.BOT} 〕━━━┈⊷
+┃✵╭──────────────
+┃✵│▸ *BOT-OWNER* : ${s.OWNER_NAME}
+┃✵│▸ *COMMANDER* : ${nomAuteurMessage} 
+│✰╰──────────────★
+┃✵│▸ *TODAY* : ${date}
+┃✵│▸ *PREFIX* : ${s.PREFIXE}
+┃✵│▸ *WORKTYPE* : ${mode} mode
+┃✵│▸ *PLUGINS* : ${cm.length} 
+┃✵│▸ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+┃✵│▸ *RUNNING ON* : ${os.platform()}
+┃✵│▸ *THEME* : *𝐊𝐄𝐈𝐓𝐇*
+┃✵╰──────────────
+╰━━━━━━━━━━━━━━━┈⊷
+> 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐊𝐄𝐈𝐓𝐇\n${readmore}`;
     
-
 let menuMsg = `
-╭─────────────┈⊷\n
+
+ *𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒*${readmore}
 `;
-
-
 
     for (const cat in coms) {
-
-        menuMsg += `╭─────────────┈⊷ 「 ${cat}」`;
-menuMsg +=╰┬────────────┈⊷\n┌┤\n
+        menuMsg += ` ╭─────────────┈⊷`
+menuMsg += │ 「*${cat}*」` menuMsg += `╰┬────────────┈⊷\n┌┤\n`;
         for (const cmd of coms[cat]) {
-
             menuMsg += `
 ││◦➛ ${cmd}`;
-
         }
-
         menuMsg += `
-│╰────────────┈⊷\n`
-menuMsg += `╰─────────────┈⊷`
-
+│╰────────────┈⊷`
+menuMsg += `╰────────────┈⊷\n`
     }
 
-
-
-    menuMsg += `
-
-
-
-┏━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ 𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐥𝐨𝐯𝐢𝐧𝐠 𝐀𝐋𝐏𝐇𝐀-𝐌𝐃
-┗━━━━━━━━━━━━━━━━━━━━━━━━┛\n
-
-
+    menuMsg += `> 𝐑𝐄𝐆𝐀𝐑𝐃𝐒 𝐊𝐄𝐈𝐓𝐇🤝
 `;
-
-
 
    var lien = mybotpic();
 
-
-
    if (lien.match(/\.(mp4|gif)$/i)) {
-
     try {
-
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Alpha-Md*, déveloper keithkeizzah" , gifPlayback : true }, { quoted: ms });
-
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Beltahmd*, déveloper Beltah Tech" , gifPlayback : true }, { quoted: ms });
     }
-
     catch (e) {
-
-        console.log("🥵🥵 Menu error " + e);
-
-        repondre("🥵🥵 Menu error " + e);
-
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
     }
-
 } 
-
 // Vérification pour .jpeg ou .png
-
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-
     try {
-
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Alpha-Md*, déveloper keithkeizzah" }, { quoted: ms });
-
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *keithkeizzah*, déveloper Keith Tech" }, { quoted: ms });
     }
-
     catch (e) {
-
-        console.log("🥵🥵 Menu error " + e);
-
-        repondre("🥵🥵 Menu error " + e);
-
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
     }
-
 } 
-
 else {
-
     
-
     repondre(infoMsg + menuMsg);
-
     
-
 }
-
-
 
 });
