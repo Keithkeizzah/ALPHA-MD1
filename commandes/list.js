8140d6fe23261677ae126471122328b63b4b2377
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-07-26 00:08:43 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/list.js
+++ commandes/list.js
@@ -8,7 +8,7 @@ const s = require(__dirname + "/../set");
 const more = String.fromCharCode(8206)
 const readmore = more.repeat(4001)
 
-zokou({ nomCom: "menu3", categorie: "General" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "menu4", categorie: "General" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
     let { cm } = require(__dirname + "/../framework//zokou");
     var coms = {};
