d54e992ba2701709ddc3d98bc691ab2f3be91fde
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-08-30 03:24:04 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/gta.js
+++ commandes/gta.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const imgURL = 'https://api.telegram.org/file/bot7020055977:AAGb76pIutuwV__eC2q7qSQns4XFMRjd-bw/photos/file_0.jpg';
-const apiURL = https://samirxpikachuio.onrender.com/artify?url=${imgURL};
+const apiURL = 'https://samirxpikachuio.onrender.com/artify?url=${imgURL};
 const outPath = l, 'generated_image.jpg');
 zokou({
   nomCom: "gta",
@@ -12,7 +12,7 @@ zokou({
 
 }, async function getGTAImg() {
   const imageSrc = 'https://i.postimg.cc/P5cPtzZJ/FB-IMG-1720537848140.jpg';
-  const apiUrl = https://www.samirxpikachu.run.place/gta?url=${encodeURIComponent(imageSrc)};
+  const apiUrl = 'https://www.samirxpikachu.run.place/gta?url=${encodeURIComponent(imageSrc)};
 
   try {
     const response = await axios({
