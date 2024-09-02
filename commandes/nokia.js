81c511d67e096fb3c9631220fcb3a44179bcb0cf
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-02 12:27:47 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/nokia.js
+++ commandes/nokia.js
@@ -1,20 +1,412 @@
-const { zokou } = require("../framework/zokou");
-const mumaker = require("mumaker");
-
-zokou({ nomCom: "nokia", categorie: "Logo", reaction: "⚡" }, async (dest, zk, commandeOptions) => {
-  const { arg, ms, prefixe, repondre } = commandeOptions;
-
-  if (!arg[0]) {
-    repondre(`Exemple of using command:\n ${prefixe}Thunder My text`);
-    return;
-  }
-
-  const text = arg.join(" ");
-  mumaker.textpro("https://api.popcat.xyz/nokia?image=", text)
-    .then((data) => {
-      zk.sendMessage(dest, { image: { url: data.image }, caption: 'Logo by *KEITH-TECH-LOG*' }, { quoted: ms });
-    })
-    .catch((err) => {
-      console.error("An error occurred:", err);
+const {
+  zokou
+} = require("../framework/zokou");
+const {
+  default: axios
+} = require("axios");
+zokou({
+  'nomCom': "lulcat",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your text.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.popcat.xyz/lulcat?text=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
     });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "sadcat",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your text.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.popcat.xyz/sadcat?text=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "nokia",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("Your text");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.popcat.xyz/nokia?image=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "unforgivable" ,
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("example keith projects are all trash.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.popcat.xyz/unforgivable?text=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "1917",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your text");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.giftedtechnexus.co.ke/api/ephoto360/1917?text=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "cartoon",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your text.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.giftedtechnexus.co.ke/api/ephoto360/cartoonstyle?text=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "underwater",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your text.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.giftedtechnexus.co.ke/api/ephoto360/underwater?text=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "watercolor",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your text.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://api.giftedtechnexus.co.ke/api/ephoto360/watercolor?text=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "gfx",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your name.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://samirxpikachuio.onrender.com/gfx1?name=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "gfx2",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your name.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://samirxpikachuio.onrender.com/gfx2?name=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "gfx3",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your name.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://samirxpikachuio.onrender.com/gfx3?name=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "gfx4",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your name.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://samirxpikachuio.onrender.com/gfx4?name=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "gfx5",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your text.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://samirxpikachuio.onrender.com/gfx5?name=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
+});
+zokou({
+  'nomCom': "gfx6",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x22ef9c, _0x5287a8, _0x2ccd49) => {
+  const {
+    repondre: _0x2b1a81,
+    arg: _0x885e30,
+    ms: _0x4eb9fe
+  } = _0x2ccd49;
+  try {
+    if (!_0x885e30 || _0x885e30.length === 0) {
+      return _0x2b1a81("your name.");
+    }
+    const _0x30bcaa = _0x885e30.join(" ");
+    const _0x93b128 = "https://samirxpikachuio.onrender.com/gfx6?name=" + _0x30bcaa;
+    _0x5287a8.sendMessage(_0x22ef9c, {
+      'image': {
+        'url': _0x93b128
+      },
+      'caption': "*powered by ALPHA-MD*"
+    }, {
+      'quoted': _0x4eb9fe
+    });
+  } catch (_0x3fe80d) {
+    console.error("Erreur:", _0x3fe80d.message || "Une erreur s'est produite");
+    _0x2b1a81("Oops, an error occurred while processing your request");
+  }
 });
