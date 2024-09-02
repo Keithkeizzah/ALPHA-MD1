5e130e732778076127010eef7992faf8d0659263
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-02 10:03:44 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/pickupline.js
+++ commandes/pickupline.js
@@ -1,21 +1,18 @@
-const {
-  zokou
-} = require("../framework/zokou");
-const {
-  default: axios
-} = require("axios");
-zokou({
-  'nomCom': "pickupline",
-  'reaction': '💫',
-  'categorie': "FUN"
-}, async (_0x50a2f3, _0x22193f, _0x37bde5) => {
-  const {
-    repondre: _0x1358da,
-    arg: _0x1891cd,
-    ms: _0x32f549
-  } = _0x37bde5;
-  const _0x1b2d46 = await fetch("https://api.popcat.xyz/pickuplines");
-  const _0x3b4ebd = await _0x1b2d46.json();
-  await _0x1358da(_0x3b4ebd.result);
-  console.log(_0x3b4ebd.completion);
+```javascript
+const { zokou } = require("../framework/zokou");
+const axios = require("axios");
+
+zokou({ 'nomCom': "pickupline", 'reaction': '💫', 'categorie': "FUN" }, async (_0x50a2f3, _0x22193f, _0x37bde5) => {
+    const { repondre: _0x1358da, arg: _0x1891cd, ms: _0x32f549 } = _0x37bde5;
+    
+    try {
+        const response = await axios.get("https://api.popcat.xyz/pickuplines");
+        const data = response.data;
+        
+        await _0x1358da(data.result);
+        console.log(data.completion);
+    } catch (error) {
+        console.error(error);
+    }
 });
+```
