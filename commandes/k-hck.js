2f0147c0919130f476840571415870c530404616
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-06 06:44:58 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/k-hck.js
+++ commandes/k-hck.js
@@ -1,48 +1,21 @@
-const { zokou } = require("../framework/zokou");
-const conf = require("../set");
-
-// Sleep function to delay execution
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-zokou({ nomCom: "hack", categorie: "fun", reaction: "💀", filename: __filename }, async (citel) => {
-  const messages = [
-    "Injecting Malware",
-    " █ 10%",
-    " █ █ 20%",
-    " █ █ █ 30%",
-    " █ █ █ █ 40%",
-    " █ █ █ █ █ 50%",
-    " █ █ █ █ █ █ 60%",
-    " █ █ █ █ █ █ █ 70%",
-    " █ █ █ █ █ █ █ █ 80%",
-    " █ █ █ █ █ █ █ █ █ 90%",
-    " █ █ █ █ █ █ █ █ █ █ 100%",
-    "System hijacking on process..\nConnecting to Server error to find 404",
-    "Device successfully connected...\nReceiving data...",
-    "Data hijacked from device 100% completed\nKilling all evidence, killing all malwares...",
-    "HACKING COMPLETED",
-    "SENDING LOG DOCUMENTS...",
-    "SUCCESSFULLY SENT DATA AND Connection disconnected",
-    "BACKLOGS CLEARED",
-    "POWERED BY ALPHA MD",
-    "By keithkeizzah"
-  ];
-
-  let editedMessage;
-  try {
-    for (const message of messages) {
-      if (!editedMessage) {
-        // Send the initial message
-        editedMessage = await citel.send(message);
-        console.log(`Sent initial message: ${message}`);
-      } else {
-        // Edit the existing message
-        await citel.edit(editedMessage, message);
-        console.log(`Edited message to: ${message}`);
-      }
-      await sleep(1000); // Wait for 1 second
-    }
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+const {
+  zokou
+} = require("../framework/zokou");
+const {
+  default: axios
+} = require("axios");
+zokou({
+  'nomCom': "flirt",
+  'reaction': '😅',
+  'categorie': 'FUN'
+}, async (_0x96fecf, _0x4960ba, _0x392b0f) => {
+  const {
+    repondre: _0x2846aa,
+    arg: _0xcf7a06,
+    ms: _0x557cbd
+  } = _0x392b0f;
+  const _0x3dfb6f = await fetch("https://shizoapi.onrender.com/api/texts/flirt?apikey=");
+  const _0xb224c6 = await _0x3dfb6f.json();
+  await _0x2846aa(_0xb224c6.result);
+  console.log(_0xb224c6.completion);
 });
