e349279f1d186a5d96f8fde2897fdb0168c2419a
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-08 20:41:45 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/pastebin.js
+++ commandes/pastebin.js
@@ -2,69 +2,67 @@ const { zokou } = require("../framework/zokou");
 const axios = require("axios");
 const { Sticker, StickerTypes } = require("wa-sticker-formatter");
 
+// Replace with your actual bot token and base URL
+const botToken = 'bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4';
+const baseUrl = `https://api.telegram.org/${botToken}`;
+
 zokou({
-  'nomCom': 'telesticker',
-  'categorie': 'Mods'
-}, async (_0x2e51e1, _0x5b32f3, _0x2435f3) => {
-  const {
-    ms: messageSender,
-    repondre: reply,
-    arg: args,
-    nomAuteurMessage: messageAuthor,
-    superUser: isSuperUser
-  } = _0x2435f3;
-
-  if (!isSuperUser) {
-    reply("Only Mods can use this command");
+  nomCom: 'telesticker',
+  categorie: 'Mods'
+}, async (ms, repondre, arg, nomAuteurMessage, superUser) => {
+  if (!superUser) {
+    repondre("Only Mods can use this command");
     return;
   }
 
-  if (!args[0]) {
-    reply("Please provide a Telegram sticker link.");
+  if (!arg[0]) {
+    repondre("Please provide a Telegram sticker link");
     return;
   }
 
-  const stickerLink = args.join(" ");
+  const stickerLink = arg.join(" ");
   const stickerSetName = stickerLink.split("/addstickers/")[1];
-  const apiUrl = `https://api.telegram.org/botYOUR_BOT_TOKEN/getStickerSet?name=${encodeURIComponent(stickerSetName)}`;
+  const requestUrl = `https://api.telegram.org/bot${botToken}/getStickerSet?name=${encodeURIComponent(stickerSetName)}`;
 
   try {
-    const response = await axios.get(apiUrl);
+    const response = await axios.get(requestUrl);
     const stickerSet = response.data.result;
+
     const isAnimated = stickerSet.is_animated || stickerSet.is_video;
     const stickerType = isAnimated ? "animated sticker" : "not animated sticker";
+    const statusMessage = `
+      Alpha-stickers-dl
+      *Name:* ${stickerSet.name}
+      *Type:* ${stickerType}
+      *Length:* ${stickerSet.stickers.length}
+      Downloading...
+    `;
 
-    const message = `Alpha-stickers-dl\n\n*Name:* ${stickerSet.name}\n*Type:* ${stickerType}\n*Length:* ${stickerSet.stickers.length}\n\nDownloading...`;
-    await reply(message);
+    await repondre(statusMessage);
 
     for (const sticker of stickerSet.stickers) {
-      const fileResponse = await axios.get(`https://api.telegram.org/botYOUR_BOT_TOKEN/getFile?file_id=${sticker.file_id}`);
+      const fileResponse = await axios.get(`https://api.telegram.org/bot${botToken}/getFile?file_id=${sticker.file_id}`);
       const filePath = fileResponse.data.result.file_path;
-
-      const fileBufferResponse = await axios({
-        method: "get",
-        url: `https://api.telegram.org/file/botYOUR_BOT_TOKEN/${filePath}`,
-        responseType: "arraybuffer"
+      const fileData = await axios({
+        method: 'get',
+        url: `https://api.telegram.org/file/bot${botToken}/${filePath}`,
+        responseType: 'arraybuffer'
       });
 
-      const stickerObject = new Sticker(fileBufferResponse.data, {
-        pack: messageAuthor,
+      const stickerObject = new Sticker(fileData.data, {
+        pack: nomAuteurMessage,
         author: "ALPHA-MD",
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
         id: "12345",
-        quality: 50,
+        quality: 50,  // Adjust quality as needed
         background: "#000000"
       });
 
       const stickerBuffer = await stickerObject.toBuffer();
-      await _0x5b32f3.sendMessage(_0x2e51e1, {
-        sticker: stickerBuffer
-      }, {
-        quoted: messageSender
-      });
+      await ms.sendMessage(ms, { sticker: stickerBuffer }, { quoted: ms });
     }
   } catch (error) {
-    reply("An error occurred:\n" + error.message);
+    repondre(`An error occurred: ${error.message}`);
   }
 });
