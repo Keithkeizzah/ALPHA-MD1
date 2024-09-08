55645cb5f6893befeb9a3324a3c00c3b7685c8ea
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-08 19:50:01 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/pastebin.js
+++ commandes/pastebin.js
@@ -1,26 +1,70 @@
-const { zokou } = require('../framework/zokou');
-const axios = require('axios');
-const fs = require('fs');
+const { zokou } = require("../framework/zokou");
+const axios = require("axios");
+const { Sticker, StickerTypes } = require("wa-sticker-formatter");
 
-zokou({nomCom:"GTAImg", reaction:"📡", categorie:"IA"}, async (dest, zk, commandeOptions) => {
+zokou({
+  'nomCom': 'telesticker',
+  'categorie': 'Mods'
+}, async (_0x2e51e1, _0x5b32f3, _0x2435f3) => {
+  const {
+    ms: messageSender,
+    repondre: reply,
+    arg: args,
+    nomAuteurMessage: messageAuthor,
+    superUser: isSuperUser
+  } = _0x2435f3;
 
-  async function getGTAImg() {
-    const imageSrc = 'https://i.postimg.cc/P5cPtzZJ/FB-IMG-1720537848140.jpg';
-    const apiUrl = `https://www.samirxpikachu.run.place/gta?url=${encodeURIComponent(imageSrc)}`;
-
-    try {
-      const response = await axios({
-        method: 'get',
-        url: apiUrl,
-        responseType: 'arraybuffer',
-      });
-
-      fs.writeFileSync('gta.jpg', response.data);
-      console.log('Saved as gta.jpg');
-    } catch (error) {
-      console.error('Error:', error);
-    }
+  if (!isSuperUser) {
+    reply("Only Mods can use this command");
+    return;
   }
 
-  getGTAImg();
+  if (!args[0]) {
+    reply("Please provide a Telegram sticker link.");
+    return;
+  }
+
+  const stickerLink = args.join(" ");
+  const stickerSetName = stickerLink.split("/addstickers/")[1];
+  const apiUrl = `https://api.telegram.org/botYOUR_BOT_TOKEN/getStickerSet?name=${encodeURIComponent(stickerSetName)}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const stickerSet = response.data.result;
+    const isAnimated = stickerSet.is_animated || stickerSet.is_video;
+    const stickerType = isAnimated ? "animated sticker" : "not animated sticker";
+
+    const message = `Alpha-stickers-dl\n\n*Name:* ${stickerSet.name}\n*Type:* ${stickerType}\n*Length:* ${stickerSet.stickers.length}\n\nDownloading...`;
+    await reply(message);
+
+    for (const sticker of stickerSet.stickers) {
+      const fileResponse = await axios.get(`https://api.telegram.org/botYOUR_BOT_TOKEN/getFile?file_id=${sticker.file_id}`);
+      const filePath = fileResponse.data.result.file_path;
+
+      const fileBufferResponse = await axios({
+        method: "get",
+        url: `https://api.telegram.org/file/botYOUR_BOT_TOKEN/${filePath}`,
+        responseType: "arraybuffer"
+      });
+
+      const stickerObject = new Sticker(fileBufferResponse.data, {
+        pack: messageAuthor,
+        author: "ALPHA-MD",
+        type: StickerTypes.FULL,
+        categories: ['🤩', '🎉'],
+        id: "12345",
+        quality: 50,
+        background: "#000000"
+      });
+
+      const stickerBuffer = await stickerObject.toBuffer();
+      await _0x5b32f3.sendMessage(_0x2e51e1, {
+        sticker: stickerBuffer
+      }, {
+        quoted: messageSender
+      });
+    }
+  } catch (error) {
+    reply("An error occurred:\n" + error.message);
+  }
 });
