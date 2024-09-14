6d469f86a21be6cda1ca05704452f1f63439fcf4
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-15 02:06:08 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃⊂(●ᆺ●)⊃

--- commandes/m-logo.js
+++ commandes/m-logo.js
@@ -1,16 +1,220 @@
 const { zokou } = require("../framework/zokou");
 var mumaker = require("mumaker");
-zokou({nomCom:"birthday1",categorie:"Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"birthday1",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
 
 
   let {ms,arg,prefixe,repondre}=commandeOptions;
   try{
       if(!arg||arg=="")
       {
-        repondre(prefixe+"arena Alpha Md");return;
+        repondre(prefixe+"birthday1 Alpha Md");return;
       }
 
     var lien="https://en.ephoto360.com/birthday-cake-96.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"birthday2",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"birthday2 Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/write-name-on-pubg-birthday-cake-images-522.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"birthday3",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"birthday3 Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/write-name-on-flower-birthday-cake-pics-472.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"birthday4",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"birthday4 Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/create-birthday-cards-by-3d-names-373.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"birthday5",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"birthday5 Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/create-birthday-cards-by-3d-names-373.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"birthday6",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"birthday6 Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/heart-shaped-rose-birthday-cake-with-name-edit-469.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"birthday7",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"birthday7 Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/write-name-on-red-rose-birthday-cake-images-462.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"comic",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"comic Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/create-online-3d-comic-style-text-effects-817.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"zodiac",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"zodiac Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/free-zodiac-online-logo-maker-491.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"deadpool",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"deadpool Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/create-text-effects-in-the-style-of-the-deadpool-logo-818.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"avenger",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"avenger Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/create-logo-3d-style-avengers-online-427.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"wolf",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"wolf Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/create-logo-avatar-wolf-galaxy-online-366.html";
+
+    var img = await mumaker.ephoto(lien,arg.join(' '));
+   repondre("processing ...")
+    await zk.sendMessage(dest,{image:{url:img.image},caption:" *Powered by Alpha Md*"},{quoted:ms})
+  }catch(e){repondre(e)}
+})
+zokou({nomCom:"bokeh",categorie:"Modern-Logo",reaction:"🥵"},async(dest,zk,commandeOptions)=>{
+
+
+  let {ms,arg,prefixe,repondre}=commandeOptions;
+  try{
+      if(!arg||arg=="")
+      {
+        repondre(prefixe+"bokeh Alpha Md");return;
+      }
+
+    var lien="https://en.ephoto360.com/bokeh-text-effect-86.html";
 
     var img = await mumaker.ephoto(lien,arg.join(' '));
    repondre("processing ...")
