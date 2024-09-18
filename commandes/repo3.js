c7e89a4a920a54fc6d5e5a3a277be4badad3b58f
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-18 16:51:26 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/repo3.js
+++ commandes/repo3.js
@@ -1,16 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
+const fetch = require("node-fetch"); // Ensure fetch is imported
 
-zokou({ nomCom: "repo3", catégorie:"Général", reaction: "🖕", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "repo3", catégorie: "Général", reaction: "🖕", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/keithkeizzah/ALPHA-MD1';
   const img = 'https://i.imgur.com/hRP6xPl.jpeg';
 
   try {
-    const response = await fetch(githubRepo);
+    const response = await fetch(githubRepo, {
+      headers: {
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    });
     const data = await response.json();
 
-    if (data) {
+    if (response.ok) {
       const repoInfo = {
         stars: data.stargazers_count,
         forks: data.forks_count,
@@ -19,24 +24,22 @@ zokou({ nomCom: "repo3", catégorie:"Général", reaction: "🖕", nomFichier: _
       };
 
       const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
-      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(repoInfo.lastUpdate).toLocaleDateString('en-GB');
 
-      const gitdata = `*Hello,,,👋This is 𝐀𝐋𝐏𝐇𝐀-𝐌𝐃*\n the best bot in the universe developed by Kᴇɪᴛʜ Kᴇɪᴢᴢᴀʜ,,fork and give a star 🌟 to my repo
-
-✞ *REPOSITORY:* ${data.html_url}
- ✞ *UPDATE ON:* ${repoInfo.lastUpdate}
-
-   ✞*Visitors:* ${repoInfo.visitors}
-  ✞ *Stars:* ${repoInfo.stars}
-  ✞ *Forks:* ${repoInfo.forks}
-  ✞ *Release date:* ${releaseDate}
-  ✞ *Owner:* *keithkeizzah*
-  ✞ *session:* *https://keith-sessions-pi5z.onrender.com* 
- >Regards keithkeizzah `;
+      const gitdata = `*Hello ${nomAuteurMessage},,,👋This is 𝐀𝐋𝐏𝐇𝐀-𝐌𝐃*\n the best bot in the universe developed by Kᴇɪᴛʜ Kᴇɪᴢᴢᴀʜ,,fork and give a star 🌟 to my repo
+╭────────────────────☆
+│❍ *Owner:* *${repoInfo.owner}*
+│❍ *Updated on:* ${lastUpdateDate}
+│❍ *Stars:* ${repoInfo.stars}
+│❍ *Forks:* ${repoInfo.forks}
+│❍ *Release date:* ${releaseDate}
+│❍ *Repo:* ${data.html_url}
+│❍ *session:* https://keith-sessions-pi5z.onrender.com
+╰─────────────────────☆`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
     } else {
-      console.log("Could not fetch data");
+      console.log("Could not fetch data: ", data.message);
     }
   } catch (error) {
     console.log("Error fetching data:", error);
