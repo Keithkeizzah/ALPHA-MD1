61528fd0f1e0a07acfe16d18a127c0d05d89c52f
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-21 08:32:07 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/stalks.js
+++ commandes/stalks.js
@@ -1,34 +1,31 @@
-const goat = require("api-dylux");
 const { zokou } = require("../framework/zokou");
+const axios = require("axios");
+const Genius = require("genius-lyrics");
+
+const geniusClient = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
 
 zokou({
-  'nomCom': "instastalk",
-  'reaction': '📷',
-  'categorie': "STALKERS"
-}, async (_0x1586d0, _0xf413a6, _0x5c156a) => {
-  const {
-    repondre: _0x3dfa33,
-    prefixe: _0x33b470,
-    arg: _0x80864c,
-    ms: _0x2975a2
-  } = _0x5c156a;
-
-  const username = _0x80864c.join(" ");
-  if (!username) {
-    return _0x3dfa33("Please provide a valid Instagram username like: " + _0x33b470 + "instastalk keizzah_keith");
+  nomCom: "instastalk",
+  reaction: '🎎',
+  categorie: "General"
+}, async (context, message, params) => {
+  const { repondre: respond, arg, ms } = params;
+  const reference = arg.join(" ");
+  
+  if (!reference) {
+    return respond("Please specify the username");
   }
-
+  
   try {
-    const response = await fetch(`https://www.noobs-api.000.pe/dipto/instainfo?username=${encodeURIComponent(username)}`);
-    const data = await response.json();
-
-    if (!data.data || !data.data.user_info) {
-      return _0x3dfa33("Couldn't fetch the data for username: " + username + ". Please check the username and try again.");
+    const response = await fetch(https://www.noobs-api.000.pe/dipto/instainfo?username=${encodeURIComponent(username)}`);
+    
+    if (!response.ok) {
+      return respond("Invalid username");
     }
-
-    const userInfo = data.data.user_info;
-    const message = `
-┌──「 *ALPHA INSTAGRAM STALK* 
+    
+    const data = await response.json();
+    const messageText = `
+   ┌──「 *ALPHA INSTAGRAM STALK* 
 ▢ *🔖Name:* ${userInfo.full_name || 'Unknown'}
 ▢ *🔖Username:* ${userInfo.username || "Unknown"}
 ▢ *👥Followers:* ${userInfo.followers || 'Unknown'}
@@ -37,16 +34,10 @@ zokou({
 ▢ *🔗 External Link:* ${userInfo.external_url || "No Link"}
 ▢ *🔗 Profile Link:* https://instagram.com/${userInfo.username || "unknown"}
 └────────────`;
-
-    await _0xf413a6.sendMessage(_0x1586d0, {
-      'image': {
-        'url': userInfo.profile_pic_url
-      },
-      'caption': message
-    }, {
-      'quoted': _0x2975a2
-    });
+    
+    await respond(messageText);
   } catch (error) {
-    return _0x3dfa33("Error: " + error.toString());
+    console.error(error);
+    await respond("An error occurred .");
   }
 });
