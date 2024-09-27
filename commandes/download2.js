874d4e7ff99580663efcacec45b804b0d5ca3956
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-27 12:14:29 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/download2.js
+++ commandes/download2.js
@@ -1,147 +1,80 @@
 const { zokou } = require("../framework/zokou");
 const yts = require('yt-search');
-const fs = require('fs');
-const fetch = require('node-fetch');
-
+const fetch = require('node-fetch'); // Ensure fetch is imported
 const BaseUrl = 'https://api-gifted-test-460bb726857c.herokuapp.com';
-const giftedapikey = 'giftedtechk';
+const giftedapikey = 'gifteddevskk';
 
-zokou({
-  nomCom: "song",
-  categorie: "Search",
-  reaction: "💿"
-}, async (origineMessage, zk, commandeOptions) => {
+const sendMedia = async (dest, zk, mediaInfo, mediaUrl, quotedMessage, mediaType) => {
+  await zk.sendMessage(dest, mediaInfo, { quoted: quotedMessage });
+  await zk.sendMessage(dest, mediaType === 'audio' ? { audio: { url: mediaUrl }, mimetype: 'audio/mp4' } : { document: { url: mediaUrl }, mimetype: 'video/mp4' }, { quoted: quotedMessage });
+};
+
+const handleMediaSearch = async (dest, zk, commandeOptions, mediaType) => {
   const { ms, repondre, arg } = commandeOptions;
 
   if (!arg[0]) {
-    repondre("Hello _*${m.pushName}*_ , Please provide song name, eg *.play already dead by Juice WRLD*.");
+    repondre("Please insert a song/video name.");
     return;
   }
 
   try {
     const topo = arg.join(" ");
-    const search = await yts(topo);
-    const videos = search.videos;
+    const searchResults = await yts(topo);
+    const videos = searchResults.videos;
 
-    if (videos && videos.length > 0) {
+    if (videos.length > 0) {
       const videoUrl = videos[0].url;
-      const apiResponse = await fetch(`${BaseUrl}/api/download/ytmp3?url=${encodeURIComponent(videoUrl)}&apikey=${giftedapikey}`);
+      const downloadUrl = `${BaseUrl}/api/download/ytm${mediaType === 'audio' ? 'p3' : 'p4'}?url=${encodeURIComponent(videoUrl)}&apikey=${giftedapikey}`;
+      const apiResponse = await fetch(downloadUrl);
       const apiResult = await apiResponse.json();
 
       if (apiResult.status === 200 && apiResult.success) {
-        const audioUrl = apiResult.result.download_url;
+        const mediaDlUrl = apiResult.result.download_url;
+        const videoInfo = videos[0];
+
         const infoMess = {
-          image: { url: videos[0].thumbnail },
-          caption: `*ALPHA-MD SONG PLAYER*\n
-╭───────────────◆
-│✞ *Title:* ${apiResult.result.title}
-│✞ *Quality:* ${apiResult.result.type}
-│✞ *Duration:* ${videos[0].timestamp}
-│✞ *Viewers:* ${videos[0].views}
-│✞ *Uploaded:* ${videos[0].ago}
-│✞ *Artist:* ${videos[0].author.name}
-╰────────────────◆
-⦿ *Direct Link:* ${audioUrl}
-╭────────────────◆
-│ *_Powered by keithkeizzah._*
-╰─────────────────◆`
+          image: { url: videoInfo.thumbnail },
+          caption: `*ALPHA-MD ${mediaType === 'audio' ? 'SONG' : 'VIDEO'} PLAYER*\n` +
+                   `╭───────────────𝄡\n` +
+                   `│𝄟 *Title:* ${apiResult.result.title}\n` +
+                   `│𝄟 *Quality:* ${apiResult.result.type}\n` +
+                   `│𝄟 *Duration:* ${videoInfo.timestamp}\n` +
+                   `│𝄟 *Viewers:* ${videoInfo.views}\n` +
+                   `│𝄟 *Uploaded:* ${videoInfo.ago}\n` +
+                   `│𝄟 *Artist:* ${videoInfo.author.name}\n` +
+                   `╰────────────────𝄡\n` +
+                   `𝄤 *Direct YtLink:* ${videoUrl}\n\n` +
+                   `╭────────────────𝄡\n` +
+                   `│ *_Regards keithkeizzah._*\n` +
+                   `╰─────────────────𝄡`
         };
 
-        await zk.sendMessage(origineMessage, infoMess, { quoted: ms });
-        await zk.sendMessage(origineMessage, { audio: { url: audioUrl }, mimetype: 'audio/mp4' }, { quoted: ms });
+        await sendMedia(dest, zk, infoMess, mediaDlUrl, ms, mediaType);
         repondre('Download Success...');
       } else {
-        repondre('Failed to download audio. Please try again later.');
+        repondre('Failed to download the media. Please try again later.');
       }
     } else {
-      repondre('No videos found.');
+      repondre('No media found.');
     }
   } catch (error) {
-    console.error('Error while searching or downloading the video:', error);
-    repondre('An error occurred while searching or downloading the video.');
+    console.error('Error from API:', error);
+    repondre('An error occurred while searching or downloading the media.');
   }
+};
+
+zokou({ nomCom: "play", categorie: "Search", reaction: "💿" }, (dest, zk, commandeOptions) => {
+  handleMediaSearch(dest, zk, commandeOptions, 'audio');
 });
 
-zokou({
-  nomCom: "video",
-  categorie: "Search",
-  reaction: "🎥"
-}, async (origineMessage, zk, commandeOptions) => {
-  const { arg, ms, repondre } = commandeOptions;
-
-  if (!arg[0]) {
-    repondre("Hello _*${m.pushName}*_ , Please provide song name, eg *.video already dead by Juice WRLD*.");
-    return;
-  }
-
-  try {
-    const topo = arg.join(" ");
-    const search = await yts(topo);
-    const videos = search.videos;
-
-    if (videos && videos.length > 0) {
-      const videoUrl = videos[0].url;
-      const apiResponse = await fetch(`${BaseUrl}/api/download/ytmp4?url=${encodeURIComponent(videoUrl)}&apikey=${giftedapikey}`);
-      const apiResult = await apiResponse.json();
-
-      if (apiResult.status === 200 && apiResult.success) {
-        const videoDownloadUrl = apiResult.result.download_url;
-        const infoMess = {
-          image: { url: videos[0].thumbnail },
-          caption: `*ALPHA-MD VIDEO DOWNLOADER*\n
-╭───────────────◆
-│᳆ *Title:* ${apiResult.result.title}
-│᳆ *Duration:* ${videos[0].timestamp}
-│᳆ *Viewers:* ${videos[0].views}
-│᳆ *Uploaded:* ${videos[0].ago}
-│᳆ *Author:* ${videos[0].author.name}
-╰────────────────◆`
-        };
-
-        await zk.sendMessage(origineMessage, infoMess, { quoted: ms });
-        await zk.sendMessage(origineMessage, { video: { url: videoDownloadUrl }, caption: "╭───────────────◆\n│ *ALPHA-MD DOWNLOADER*\n╰────────────────◆" }, { quoted: ms });
-        repondre('Download Success...');
-      } else {
-        repondre('Failed to download video. Please try again later.');
-      }
-    } else {
-      repondre('No video found.');
-    }
-  } catch (error) {
-    console.error('Error while searching or downloading the video:', error);
-    repondre('An error occurred while searching or downloading the video.');
-  }
+zokou({ nomCom: "videodoc", categorie: "Search", reaction: "🎥" }, (dest, zk, commandeOptions) => {
+  handleMediaSearch(dest, zk, commandeOptions, 'video');
 });
 
-zokou({
-  nomCom: "mygroups",
-  categorie: "User",
-  reaction: "💿"
-}, async (senn, zk, commandeOptions) => {
-  const { ms, repondre } = commandeOptions;
+zokou({ nomCom: "video", categorie: "Search", reaction: "🎥" }, (dest, zk, commandeOptions) => {
+  handleMediaSearch(dest, zk, commandeOptions, 'video');
+});
 
-  try {
-    const getGroupzs = await zk.groupFetchAllParticipating();
-    const groupzs = Object.entries(getGroupzs).map((entry) => entry[1]);
-    const groupList = groupzs.map((v) => ({
-      name: v.subject,
-      id: v.id,
-      members: v.participants.length,
-    }));
-
-    if (groupList.length === 0) {
-      repondre("You are not part of any groups.");
-      return;
-    }
-
-    let jackhuh = `*GROUPS YOU ARE IN*\n\n`;
-    groupList.forEach(group => {
-      jackhuh += `*GROUP NAME:* ${group.name}\n*MEMBERS:* ${group.members}\n*GROUP ID:* ${group.id}\n\n`;
-    });
-
-    repondre(jackhuh);
-  } catch (error) {
-    console.error('Error fetching groups:', error);
-    repondre('An error occurred while fetching group details.');
-  }
+zokou({ nomCom: "song", categorie: "Search", reaction: "🎵" }, (dest, zk, commandeOptions) => {
+  handleMediaSearch(dest, zk, commandeOptions, 'audio');
 });
