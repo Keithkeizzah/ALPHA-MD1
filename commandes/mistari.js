f6d6871cba5166b119c5e89a4a9944f44c3aa24b
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-30 21:06:09 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/mistari.js
+++ commandes/mistari.js
@@ -1,39 +1,111 @@
-const { zokou } = require("../framework/zokou");
-const axios = require("axios");
-
+const {
+  zokou
+} = require("../framework/zokou");
+const {
+  default: axios
+} = require("axios");
 zokou({
-  nomCom: "mistari",
-  reaction: '✨',
-  categorie: "Search"
-}, async (messageId, chatId, { repondre, arg, ms }) => {
+  'nomCom': "chat",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x519f4b, _0x75f67d, _0x1ec287) => {
+  const {
+    repondre: _0x2a6e71,
+    arg: _0x4be2d3,
+    ms: _0xc63a20
+  } = _0x1ec287;
   try {
-    if (!arg || arg.length === 0) {
-      return repondre("Please provide me the song name.");
+    if (!_0x4be2d3 || _0x4be2d3.length === 0x0) {
+      return _0x2a6e71("Please ask a question.");
     }
-
-    const songName = arg.join(" ");
-    const searchResults = await Client.songs.search(songName);
-    const firstResult = searchResults[0];
-
-    if (!firstResult) {
-      return repondre(`I did not find any lyrics for "${songName}". Try searching a different song.`);
-    }
-
-    const query = firstResult.title; // Use the title of the first search result
-    const response = await axios.get(`https://samirxpikachuio.onrender.com/lyrics?query=${encodeURIComponent(query)}`);
-    
-    if (response.data && response.data.lyrics) {
-      const lyrics = response.data.lyrics;
-      await chatId.sendMessage(messageId, {
-        text: lyrics
-      }, {
-        quoted: ms
-      });
+    const _0x5d0a29 = _0x4be2d3.join(" ");
+    const _0x351572 = await fetch(https://hercai.onrender.com/v3/hercai?question=" + _0x3bd168);
+    const _0x4d66b9 = await _0x351572.json();
+    if (_0x4d66b9 && _0x4d66b9.result) {
+      const _0x12cd9d = _0x4d66b9.result;
+      const _0x29d2ee = _0x12cd9d.match(/```([\s\S]*?)```/);
+      const _0x18ee4a = [{
+        'name': "cta_url",
+        'buttonParamsJson': JSON.stringify({
+          'display_text': "FOLLOW OUR SUPPORT CHANNEL",
+          'url': "https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47"
+        })
+      }];
+      if (_0x29d2ee) {
+        const _0x5b6d25 = _0x29d2ee[0x1];
+        _0x18ee4a.unshift({
+          'name': "cta_copy",
+          'buttonParamsJson': JSON.stringify({
+            'display_text': "📋 COPY YOUR CODE",
+            'id': "copy_code",
+            'copy_code': _0x5b6d25
+          })
+        });
+        const _0x272274 = generateWAMessageFromContent(_0x519f4b, {
+          'viewOnceMessage': {
+            'message': {
+              'messageContextInfo': {
+                'deviceListMetadata': {},
+                'deviceListMetadataVersion': 0x2
+              },
+              'interactiveMessage': proto.Message.InteractiveMessage.create({
+                'body': proto.Message.InteractiveMessage.Body.create({
+                  'text': _0x12cd9d
+                }),
+                'footer': proto.Message.InteractiveMessage.Footer.create({
+                  'text': "> *POWERED BY ALPHA-MD*"
+                }),
+                'header': proto.Message.InteractiveMessage.Header.create({
+                  'title': '',
+                  'subtitle': '',
+                  'hasMediaAttachment': false
+                }),
+                'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.create({
+                  'buttons': _0x18ee4a
+                })
+              })
+            }
+          }
+        }, {});
+        await _0x75f67d.relayMessage(_0x519f4b, _0x272274.message, {
+          'messageId': _0x272274.key.id
+        });
+      } else {
+        const _0x3f8b3f = generateWAMessageFromContent(_0x519f4b, {
+          'viewOnceMessage': {
+            'message': {
+              'messageContextInfo': {
+                'deviceListMetadata': {},
+                'deviceListMetadataVersion': 0x2
+              },
+              'interactiveMessage': proto.Message.InteractiveMessage.create({
+                'body': proto.Message.InteractiveMessage.Body.create({
+                  'text': _0x12cd9d
+                }),
+                'footer': proto.Message.InteractiveMessage.Footer.create({
+                  'text': "> *POWERED BY ALPHA-MD*"
+                }),
+                'header': proto.Message.InteractiveMessage.Header.create({
+                  'title': '',
+                  'subtitle': '',
+                  'hasMediaAttachment': false
+                }),
+                'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.create({
+                  'buttons': _0x18ee4a
+                })
+              })
+            }
+          }
+        }, {});
+        await _0x75f67d.relayMessage(_0x519f4b, _0x3f8b3f.message, {
+          'messageId': _0x3f8b3f.key.id
+        });
+      }
     } else {
-      repondre("Sorry, I couldn't find the lyrics for that song.");
+      throw new Error("Invalid response from the GPT API.");
     }
-  } catch (error) {
-    console.error(error);
-    repondre("An error occurred while fetching the lyrics.");
+  } catch (_0x1a7921) {
+    console.error("Error getting GPT response:", _0x1a7921.message);
+    _0x2a6e71("Error getting response from GPT.");
   }
 });
