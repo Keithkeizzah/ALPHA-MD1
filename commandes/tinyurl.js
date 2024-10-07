5117ee291a6603d8fcce2bc25dc22d560fd31105
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-07 10:27:23 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/tinyurl.js
+++ commandes/tinyurl.js
@@ -1,22 +1,57 @@
-const { zokou } = require("../framework/zokou");
-const axios = require('axios');
+'use strict';
 
-zokou({ nomCom: "tinyurl", categorie: "General", reaction: "👨🏿‍💻" }, async (origineMessage, zk, commandeOptions) => {
-    const { msgRepondu, repondre } = commandeOptions;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-    // Check if a URL is provided in the message
-    const urlToShorten = msgRepondu?.urlToshorten;
+const { zokou } = require('../framework/zokou');
 
-    if (!urlToShorten) {
-        repondre('Please mention a URL to shorten.');
-        return;
+zokou({
+  nomCom: 'users',
+  reaction: '😌'
+}, async (message, client, sendMessage) => {
+  try {
+    const response = await fetch("https://api.github.com/repos/Keithkeizzah/ALPHA-MD1");
+    const data = await response.json();
+
+    if (data) {
+      const { stargazers_count: stars, forks_count: forks } = data;
+      const totalUsers = (forks * 2) + (stars * 2);
+
+      const messageCaption = `
+        *A total of ${totalUsers} people are using ALPHA-MD.*
+
+        *${stars} people have starred it as a sign of loving it.*
+
+        *KEEP USING ALPHA BOT*
+
+        *regards keithkeizzah*
+      `;
+
+      await sendMessage(message, {
+        video: {
+          url: "https://telegra.ph/file/08f740224ed39233f92cb.mp4"
+        },
+        caption: messageCaption
+      }, {
+        contextInfo: {
+          mentionedJid: [],
+          externalAdReply: {
+            title: "THE FLASH MULTI DEVICE",
+            body: "POWERED BY ALPHA",
+            thumbnailUrl: "https://telegra.ph/file/967c663a5978c545f78d6.jpg",
+            sourceUrl: 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47',
+            mediaType: 1,
+            renderLargerThumbnail: true
+          }
+        }
+      });
+
+      await sendMessage(message, "*You can play that video and enjoy *");
+    } else {
+      console.log("Could not fetch data");
     }
-
-    try {
-        const response = await axios.get(`https://api.maskser.me/api/linkshort/cuttly?link=${encodeURIComponent(urlToShorten)}`);
-        repondre(response.data);
-    } catch (error) {
-        console.error('Error while creating the short link:', error);
-        repondre('Oops, an error occurred while shortening the link.');
-    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 });
