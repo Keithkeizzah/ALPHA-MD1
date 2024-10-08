68036695db0a14373e77010964c6120991f8ab02
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-08 19:36:13 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/population.js
+++ commandes/population.js
@@ -1,33 +1,148 @@
 const { zokou } = require("../framework/zokou");
+const yts = require("yt-search");
+const fs = require('fs');
 const axios = require("axios");
+const ytdl = require("ytdl-core");
 
 zokou({
-  nomCom: "population",
-  reaction: "🗞️",
-  categorie: "NEWS"
-}, async (repondre, arg, ms) => {
+  nomCom: "play",
+  categorie: "Search",
+  reaction: "💿"
+}, async (origineMessage, zk, commandeOptions) => {
+  const { arg, repondre } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("Which song do you want?");
+    return;
+  }
+
   try {
-    const response = await axios.get("https://bk9.fun/details/population");
-    const data = response.data;
+    const searchQuery = arg.join(" ");
+    const results = await yts(searchQuery);
+    const videos = results.videos;
 
-    if (data && data.status) {
-      const result = data.BK9; // Accessing BK9 object directly
-      const message = `*WORLDWIDE POPULATION STATISTICS:*\n\n` +
-        `- *Total Population:* ${result.current.total}\n` +
-        `- *Male Population:* ${result.current.male}\n` +
-        `- *Female Population:* ${result.current.female}\n` +
-        `- *Births This Year:* ${result.this_year.births}\n` +
-        `- *Deaths This Year:* ${result.this_year.deaths}\n` +
-        `- *Births Today:* ${result.today.births}\n` +
-        `- *Deaths Today:* ${result.today.deaths}\n\n` +
-        `> *Powered by keithkeizzah*`;
+    if (videos.length > 0) {
+      const video = videos[0];
+      const songDetails = {
+        image: { url: video.thumbnail },
+         caption: `*ALPHA-MD SONG PLAYER*\n
+╭───────────────◆
+│✞ *Title:* ${video.title}
+│✞ *Quality:* ${video.type}
+│✞ *Duration:* ${videos[0].timestamp}
+│✞ *Viewers:* ${videos[0].views}
+│✞ *Uploaded:* ${videos[0].ago}
+│✞ *Artist:* ${videos[0].author.name}
+╰────────────────◆
+⦿ *Direct YtLink:* ${video.url}
+╭────────────────◆
+u can as well join here to get your song download
+in more tracks 🤗😋 
+https://t.me/keithmd 
+use prefix {/}  example {/search dada}
+╰────────────────◆
+╭────────────────◆
+│ *_Powered by keithkeizzah._*
+╰─────────────────◆`
+      };
 
-      await repondre(message);
+      zk.sendMessage(origineMessage, songDetails, { quoted: commandeOptions.ms });
+
+      const response = await axios.get(`https://bk9.fun/download/youtube?url=${video.url}`, {
+        responseType: "stream"
+      });
+
+      const writeStream = fs.createWriteStream("audio.mp3");
+      response.data.pipe(writeStream);
+
+      writeStream.on("finish", () => {
+        zk.sendMessage(origineMessage, {
+          audio: { url: "audio.mp3" },
+          mimetype: "audio/mp4"
+        }, { quoted: commandeOptions.ms, ptt: false });
+      });
+
+      writeStream.on("error", err => {
+        console.error("Error writing file:", err);
+        repondre("Download failed");
+      });
     } else {
-      await repondre("No population data found.");
+      repondre("No video found.");
     }
   } catch (error) {
-    console.error("Error fetching population data:", error);
-    await repondre("There was an error fetching the population data. Please try again later.");
+    console.error("Error during search or download:", error);
+    repondre("Download failed");
+  }
+});
+
+zokou({
+  nomCom: "video",
+  categorie: "Search",
+  reaction: "🎥"
+}, async (origineMessage, zk, commandeOptions) => {
+  const { arg, repondre } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("Insert video name");
+    return;
+  }
+
+  const topo = arg.join(" ");
+  try {
+    const search = await yts(topo);
+    const videos = search.videos;
+
+    if (videos.length > 0) {
+      const video = videos[0];
+      const videoDetails = {
+        image: { url: video.thumbnail },
+        caption: `*ALPHA-MD VIDEO DOWNLOADER*\n
+╭───────────────◆
+│✞ *Title:* ${video.title}
+│✞ *Quality:* ${video.type}
+│✞ *Duration:* ${videos[0].timestamp}
+│✞ *Viewers:* ${videos[0].views}
+│✞ *Uploaded:* ${videos[0].ago}
+│✞ *Artist:* ${videos[0].author.name}
+╰────────────────◆
+⦿ *Direct YtLink:* ${video.url}
+╭────────────────◆
+u can as well join here to get your song download
+in more tracks 🤗😋 
+https://t.me/keithmd 
+use prefix {/}  example {/search dada}
+╰────────────────◆
+╭────────────────◆
+│ *_Powered by keithkeizzah._*
+╰─────────────────◆`
+      };
+
+      zk.sendMessage(origineMessage, videoDetails, { quoted: commandeOptions.ms });
+
+      const response = await axios.get(`https://bk9.fun/download/youtube?url=${video.url}`, {
+        responseType: "stream"
+      });
+
+      const fileStream = fs.createWriteStream("video.mp4");
+      response.data.pipe(fileStream);
+
+      fileStream.on('finish', () => {
+        zk.sendMessage(origineMessage, {
+          video: { url: "video.mp4" },
+          caption: "*𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 𝐀𝐋𝐏𝐇𝐀*",
+          gifPlayback: false
+        }, { quoted: commandeOptions.ms });
+      });
+
+      fileStream.on('error', (error) => {
+        console.error('Error writing video file:', error);
+        repondre('An error occurred while writing the video file.');
+      });
+    } else {
+      repondre('No video found');
+    }
+  } catch (error) {
+    console.error('Error during search or video download:', error);
+    repondre('An error occurred during the search or video download.');
   }
 });
