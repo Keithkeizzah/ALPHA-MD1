e8665568812b3624079c29862bfe2e0f4d221491
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-08 10:44:38 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/guess.js
+++ commandes/guess.js
@@ -1,30 +1,44 @@
 const { zokou } = require("../framework/zokou");
 const axios = require("axios");
+const Genius = require("genius-lyrics");
+
+const geniusClient = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
 
 zokou({
-  'nomCom': "guessage",
-  'reaction': '📓',
-  'categorie': "Education-corner"
-}, async (context, args, options) => {
-  const { repondre: sendResponse, arg: commandArgs, ms: message } = options;
-  const verseQuery = commandArgs.join(" ");
+  nomCom: "guessage",
+  reaction: '🎎',
+  categorie: "General"
+}, async (context, message, params) => {
+  const { repondre: respond, arg, ms } = params;
+  const reference = arg.join(" ");
   
-  if (!verseQuery) {
-    return sendResponse("Please provide a name");
+  if (!reference) {
+    return respond("Please specify the book, chapter, and verse you want to read. Example: bible john 3:16");
   }
-
-  let response = await fetch(`https://api.agify.io/?name=${name}`);
   
-  if (!response.ok) {
-    return sendResponse("Please provide an element symbol or name");
+  try {
+    const response = await fetch(`https://api.agify.io/?name=${name}`);
+    
+    if (!response.ok) {
+      return respond("provide a name example:keith");
+    }
+    
+    const data = await response.json();
+    const messageText = `
+   ᬑ *ALPHA  GUESS AGE* ᬒ
+      
+     ⧭ *_Name:_* ${data.name}
+      
+     ⧭ *_Count:_* ${data.count}
+      
+      ⧭ *_Estimated Age:_* ${data.age}
+╭────────────────◆
+│ *_Powered by keithkeizzah._*
+╰─────────────────◆ `;
+    
+    await respond(messageText);
+  } catch (error) {
+    console.error(error);
+    await respond("An error occurred while fetching the Bible passage.");
   }
-
-  let data = await response.json();
-  let formattedMessage = `
-    `*GuesAge Information:*\n
-     • *Name:* ${data.name}\n
-     • *Symbol:* ${data.count}\n
-     • *Estimated Age:* ${data.age}\n`;
-
-  await sendResponse(formattedMessage);
 });
