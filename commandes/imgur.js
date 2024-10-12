34d0e99a96e53780bb1b31e35e4a00fade4260a1
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-10-12 19:06:23 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

--- commandes/imgur.js
+++ commandes/imgur.js
@@ -1,62 +1,53 @@
 const { zokou } = require("../framework/zokou");
-const traduire = require("../framework/traduction");
-const { downloadMediaMessage,downloadContentFromMessage } =  require('@whiskeysockets/baileys');
-const fs =require("fs-extra") ;
-const axios = require('axios');  
-const FormData = require('form-data');
-const { exec } = require("child_process");
+const fs = require("fs-extra");
+const { Catbox } = require("node-catbox");
+const catbox = new Catbox();
 
-async function uploadToimgur(Path) {
-  if (!fs.existsSync(Path)) {
-      throw new Error("Fichier non existant");
+async function uploadToCatbox(filePath) {
+  if (!fs.existsSync(filePath)) {
+    throw new Error("File does not exist");
   }
 
   try {
-      const form = new FormData();
-      form.append("file", fs.createReadStream(Path));
-
-      const { data } = await axios.post("https://i.imgur.com/upload", form, {
-          headers: {
-              ...form.getHeaders(),
-          },
-      });
-
-      if (data && data[0] && data[0].src) {
-          return "https://i.imgur.com" + data[0].src;
-      } else {
-          throw new Error("Erreur lors de la récupération du lien de la vidéo");
-      }
-  } catch (err) {
-      throw new Error(String(err));
+    const response = await catbox.uploadFile({ path: filePath });
+    if (response) {
+      return response;
+    } else {
+      throw new Error("Error retrieving the file link");
+    }
+  } catch (error) {
+    throw new Error(String(error));
   }
 }
 
-zokou({ nomCom: "imgur", categorie: "General", reaction: "👨🏿‍💻" }, async (origineMessage, zk, commandeOptions) => {
-  const { msgRepondu, repondre } = commandeOptions;
+zokou({
+  nomCom: 'catbox',
+  categorie: "General",
+  reaction: '👨🏿‍💻'
+}, async (command, client, context) => {
+  const { msgRepondu: repliedMsg, repondre: reply } = context;
 
-  if (!msgRepondu) {
-      repondre('mention a image or video');
-      return;
+  if (!repliedMsg) {
+    reply("Please mention an image or video.");
+    return;
   }
 
   let mediaPath;
-
-  if (msgRepondu.videoMessage) {
-      mediaPath = await zk.downloadAndSaveMediaMessage(msgRepondu.videoMessage);
-  } else if (msgRepondu.imageMessage) {
-      mediaPath = await zk.downloadAndSaveMediaMessage(msgRepondu.imageMessage);
+  if (repliedMsg.videoMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(repliedMsg.videoMessage);
+  } else if (repliedMsg.imageMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(repliedMsg.imageMessage);
   } else {
-      repondre('mention a image or video');
-      return;
+    reply("Please mention an image or video.");
+    return;
   }
 
   try {
-      const imgur = await uploadToimgur(mediaPath);
-      fs.unlinkSync(mediaPath);  // Supprime le fichier après utilisation
-
-      repondre(imgur);
+    const fileLink = await uploadToCatbox(mediaPath);
+    fs.unlinkSync(mediaPath);
+    reply(fileLink);
   } catch (error) {
-      console.error('Erreur lors de la création du lien imgur :', error);
-      repondre('Opps error\n' + error);
+    console.error("Error while creating your URL:", error);
+    reply("Oops, an error occurred.");
   }
 });
