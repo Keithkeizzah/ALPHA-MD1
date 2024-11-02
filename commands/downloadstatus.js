949452c263a450966390a78d58950f1027919b54
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-02 09:38:35 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

---
+++ commands/downloadstatus.js
@@ -0,0 +1,54 @@
+const { zokou } = require("../framework/zokou");
+const s = require("../set");
+const fs = require('fs');
+const Heroku = require('heroku-client');
+
+// Function to get a description of an environment variable
+function getDescriptionFromEnv(varName) {
+  const filePath = "./app.json";
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const config = JSON.parse(fileContent);
+  return config.env[varName]?.description || "The environment variable description was not found.";
+}
+
+// Anti-call function setup
+zokou({
+  nomCom: 'downloadstatus',
+  categorie: "HEROKU"
+}, async (chatId, zk, context) => {
+  const { ms, repondre, superUser, auteurMessage, arg } = context;
+
+  // Check if the command is issued by the owner
+  if (!superUser) {
+    return repondre("*This command is restricted to the bot owner.* 💀");
+  }
+
+  // Validate user input and respond accordingly
+  if (!arg[0]) {
+    return repondre('Instructions:\n\nType "downloadstatus yes" to enable or "downloadstatus no" to disable.');
+  }
+
+  const option = arg.join(' ').toLowerCase();
+  switch (option) {
+    case "yes":
+      s.AUTO_DOWNLOAD_STATUS = 'yes';  // Enable Autodownloadstatus
+      responseMessage = ' has been enabled successfully.';
+      break;
+
+    case "no":
+      s.AUTO_DOWNLOAD_STATUS = 'no';  // Disable autodownload status
+      responseMessage = ' has been disabled successfully.';
+      break;
+
+    default:
+      return repondre("Please don't invent an option. Type 'downloadstatus yes' or 'downloadstatus no'.");
+  }
+
+  // Send the response message to the user
+  try {
+    await zk.sendMessage(chatId, { text: responseMessage }, { quoted: ms });
+  } catch (error) {
+    console.error("Error processing your request:", error);
+    await zk.sendMessage(chatId, { text: 'Error processing your request.' }, { quoted: ms });
+  }
+});
