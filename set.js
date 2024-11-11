a20f32a2cb74f4ecf976769996a31a0136971fc8
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-11 09:40:34 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

---
+++ set.js
@@ -0,0 +1,64 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'Scenemd',
+    PREFIXE: process.env.PREFIX || "+",
+    GITHUB : process.env.GITHUB|| 'https://github.com/Beltahk/SCENE-MRM',
+    OWNER_NAME : process.env.OWNER_NAME || "𝐁𝐞𝐥𝐭𝐚𝐡 𝐤𝐞",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254114141192",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTO_REACT: process.env.AUTO_REACTION || "non",  
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'non',              
+    CHATBOT: process.env.CHAT_BOT || "off",              
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "yes",
+    AUTO_BLOCK: process.env.BLOCK_ALL || 'yes',              
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F",
+    WEBSITE :process.env.GURL || "https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F",
+    CAPTION : process.env.CAPTION || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ SCENE-MD-V2",
+    BOT : process.env.BOT_NAME || 'SCENE_MRM',
+    URL : process.env.BOT_MENU_LINKS || '',
+    MODE: process.env.PUBLIC_MODE || "no",
+    TIMEZONE: process.env.TIMEZONE || "Africa/Nairobi", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY || null,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ETAT : process.env.PRESENCE || '',
+    //GPT : process.env.OPENAI_API_KEY || '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',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    ANTICALL: process.env.ANTICALL || 'yes',              
+    CHATBOT : process.env.PM_CHATBOT || 'no',  
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
