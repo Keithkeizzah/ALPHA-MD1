1b2f10d186191881cf9678e6c4ab494afcd95a3c
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-11 09:47:52 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

---
+++ keizzah/keith.js
@@ -0,0 +1,17 @@
+
+var tabCmds = [];
+let cm = [];
+function keith(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🌟";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { keith, Module: keith, cm };
