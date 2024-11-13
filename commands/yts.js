464d1f12dcabf8bfdc5b5ba21768f72ce09a0241
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-11-13 19:31:11 +0300

    𝐀𝐋𝐏𝐇𝐀-𝐌𝐃ヽ( ★ω★)ノᶘ⚈

---
+++ commands/yts.js
@@ -0,0 +1,31 @@
+const { keith } = require("../keizzah/keith");
+const { getytlink, ytdwn } = require("../framework/ytdl-core");
+const yts = require("yt-search");
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+
+keith({ nomCom: "yts", categorie: "Search", reaction: "✋" }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+  const query = arg.join(" ");
+
+  if (!query[0]) {
+    repondre("what do you want");
+    return;
+  }
+
+  try {
+    const info = await yts(query);
+    const resultat = info.videos;
+
+    let captions = "";
+    for (let i = 0; i < 10; i++) {
+      captions += `----------------\nTitle: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
+    }
+    captions += "\n======\n*powered by ALPHA-MD*";
+
+    // repondre(captions)
+    zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
+  } catch (error) {
+    repondre("Erreur lors de la procédure : " + error);
+  }
+});
