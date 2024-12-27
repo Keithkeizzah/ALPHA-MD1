43293591466b7e6bd80cd64feabb53eeab0cb2ae
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-27 12:31:44 +0300

    Create Alivemessage.js

---
+++ commands/Alivemessage.js
@@ -0,0 +1,61 @@
+const { keith } = require('../keizzah/keith');
+
+keith({
+  nomCom: "test",
+  aliases: ["alive", "testing"],
+  categorie: "system",
+  reaction: "⚔️"
+}, async (dest, zk, commandeOptions) => {
+  const { ms } = commandeOptions;
+
+  // Array of sound file URLs
+  const audioFiles = [
+    'https://files.catbox.moe/hpwsi2.mp3',
+    'https://files.catbox.moe/xci982.mp3',
+    'https://files.catbox.moe/utbujd.mp3',
+    'https://files.catbox.moe/w2j17k.m4a',
+    'https://files.catbox.moe/851skv.m4a',
+    'https://files.catbox.moe/qnhtbu.m4a',
+    'https://files.catbox.moe/lb0x7w.mp3',
+    'https://files.catbox.moe/efmcxm.mp3',
+    'https://files.catbox.moe/gco5bq.mp3',
+    'https://files.catbox.moe/26oeeh.mp3',
+    'https://files.catbox.moe/a1sh4u.mp3',
+    'https://files.catbox.moe/vuuvwn.m4a',
+    'https://files.catbox.moe/wx8q6h.mp3',
+    'https://files.catbox.moe/uj8fps.m4a',
+    'https://files.catbox.moe/dc88bx.m4a',
+    'https://files.catbox.moe/tn32z0.m4a'
+  ];
+
+  // Randomly pick an audio file from the list
+  const selectedAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+
+  // External URLs for references
+  const murl = 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47';
+  const img = 'https://i.imgur.com/vTs9acV.jpeg';
+
+  // Audio message object
+  const audioMessage = {
+    audio: {
+      url: selectedAudio,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,  // Marking this as a "Push-to-Talk" message
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'shizo',
+    contextInfo: {
+      externalAdReply: {
+        title: '𝗜 𝗔𝗠 𝗔𝗟𝗜𝗩𝗘 𝗠𝗢𝗧𝗛𝗘𝗥𝗙𝗨𝗖𝗞𝗘𝗥',
+        body: 'Regards Keithkeizzah',
+        thumbnailUrl: img,
+        sourceUrl: murl,
+        mediaType: 1,
+        renderLargerThumbnail: true,
+      },
+    },
+  };
+
+  // Send the audio message with the context of the original message
+  await zk.sendMessage(dest, audioMessage, { quoted: ms });
+});
